67a8a1722bcb636f24c1a122d6d52915f01a522c
Karina Penkala <marsz@poczta.onet.pl>
2022-11-25 10:09:57 +0100

    Add class Cart with toggle

--- src/js/script.js
+++ src/js/script.js
@@ -34,24 +34,24 @@
       },
     },
     // CODE ADDED START
-  cart: {
-    productList: '.cart__order-summary',
-    toggleTrigger: '.cart__summary',
-    totalNumber: `.cart__total-number`,
-    totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
-    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
-    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
-    form: '.cart__order',
-    formSubmit: '.cart__order [type="submit"]',
-    phone: '[name="phone"]',
-    address: '[name="address"]',
-  },
-  cartProduct: {
-    amountWidget: '.widget-amount',
-    price: '.cart__product-price',
-    edit: '[href="#edit"]',
-    remove: '[href="#remove"]',
-  },
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
   // CODE ADDED END
   };
 
@@ -61,9 +61,9 @@
       imageVisible: 'active',
     },
     // CODE ADDED START
-  cart: {
-    wrapperActive: 'active',
-  },
+    cart: {
+      wrapperActive: 'active',
+    },
   // CODE ADDED END
   };
 
@@ -73,17 +73,17 @@
       defaultMin: 1,
       defaultMax: 9,
     },// CODE CHANGED
-  // CODE ADDED START
-  cart: {
-    defaultDeliveryFee: 20,
-  },
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
+    },
   // CODE ADDED END
   };
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
     // CODE ADDED START
-  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   // CODE ADDED END
   };
 
@@ -286,7 +286,7 @@
       const thisWidget = this;
 
       const evebt = new Event ('updated');
-      thisWidget.element.dispatchEvent(event)
+      thisWidget.element.dispatchEvent(event);
     }
 
     initActions (){
@@ -305,6 +305,37 @@
     }
   }
     
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = []; // products added to cart
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+    const thisCart = this;
+    
+    thisCart.dom.toggleTrigger.addEventListener('click', function(){
+    thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
+
+  
+  }
+  }
   const app = {
     initMenu: function(){
       const thisApp = this; 
@@ -321,6 +352,12 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart (cartElem);
+      },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -332,9 +369,13 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     }
+
+    
   };
   app.init();
 }
+
 
 
